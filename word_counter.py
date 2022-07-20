5b1c1ef801c19fd714254875d0f28826dda272e3
befk1970 <109320353+befk1970@users.noreply.github.com>
2022-07-20 11:35:39 -0300

    Update word_counter.py
    File.closet()se cerró el último archivo abierto y se agregó el conteo de palabras y frases al principio

--- word_counter.py
+++ word_counter.py
@@ -1,3 +1,13 @@
+import os
+
+if os.path.isfile("phrases.txt"):
+    with open("phrases.txt") as archivo:
+        aux = archivo.readlines()
+        palabras=0
+        for linea in aux:
+            palabras=palabras+linea.count(' ')
+        print(f"La cantidad de palabras existentes en el archivo es de: {palabras}" y la cantidad de frases es de {len(aux)")
+
 print ("El programa termina cuando se ingresa la palabra fin")
 
 frase = input(str("Escriba una frase: "))
@@ -19,4 +29,6 @@ f.close()
 total_palabras = total_palabras - 1
 file = open ('phrases.txt')
 print(file.read())
-print(f"Usted ha ingresado un total de {total_palabras} palabras")
+print(f"Usted ha ingresado un total de {total_palabras} palabras ")
+file.close()                                                                                                                   
+      
