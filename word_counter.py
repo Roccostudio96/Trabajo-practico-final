ca44cbd4d1c2cace406eee5a7bea630f32b41467
CynSol <108904886+CynSol@users.noreply.github.com>
2022-07-20 10:40:44 -0300

    Create word_counter.py

---
+++ word_counter.py
@@ -0,0 +1,22 @@
+print ("El programa termina cuando se ingresa la palabra fin")
+
+frase = input(str("Escriba una frase: "))
+total_palabras = frase.count(' ') + 1
+#print (f"La frase tiene {total_palabras} palabras")
+
+f = open('phrases.txt', 'a')
+f.write('\n' + frase )
+
+while (frase != 'fin'):
+    frase = input(str("Escriba otra frase: "))
+    total_palabras_otra_frase = frase.count(' ') + 1
+    print (f"La frase tiene {total_palabras_otra_frase} palabras")
+    f.write('\n' + frase )
+    total_palabras = total_palabras + total_palabras_otra_frase
+
+f.close()
+
+total_palabras = total_palabras - 1
+file = open ('phrases.txt')
+print(file.read())
+print(f"Usted ha ingresado un total de {total_palabras} palabras")
