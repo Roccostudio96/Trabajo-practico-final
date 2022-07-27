7addc6fc927ef4f61f649dd17718de3d08eda033
CynSol <108904886+CynSol@users.noreply.github.com>
2022-07-27 07:57:07 -0300

    Update adress_book.py

--- adress_book.py
+++ adress_book.py
@@ -2,19 +2,22 @@ import csv
 import uuid
 from datetime import datetime
 
-ID = uuid.uuid1()
-
-Nombre = input("Ingrese su nombre: ")
-Direccion = input("Ingrese su direccion: ")
-Telefono = input("Ingrese su numero de telefono: ")
-
-Fecha = datetime.today().strftime('%d-%m-%Y')
-
 with open('Adresses.csv', 'a') as csvfile:
+    
     fieldnames = ["ID", "Nombre", "Direccion", "Telefono", "Fecha"]
     writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-    
     writer.writeheader()
-    writer.writerow({"ID" : ID, "Nombre" : Nombre.upper(), "Direccion" : Direccion.upper(), "Telefono" : Telefono, "Fecha" : Fecha})
+    
+    cantidad = int(input("Cuantos registros tiene para ingresar?: "))
+    
+    for i in range (cantidad,0,-1):
+    
+        ID = uuid.uuid4()
+        Nombre = input("Ingrese su nombre: ")
+        Direccion = input("Ingrese su direccion: ")
+        Telefono = input("Ingrese su numero de telefono: ")
+        Fecha = datetime.today().strftime('%d-%m-%Y')
+        
+        writer.writerow({"ID" : ID, "Nombre" : Nombre.upper(), "Direccion" : Direccion.upper(), "Telefono" : Telefono, "Fecha" : Fecha})
 
 print("Datos Insertados Correctamente")    
