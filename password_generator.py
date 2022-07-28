b0be5b6a1bfdf38af8a826ad2c200b96be41d1ae
befk1970 <109320353+befk1970@users.noreply.github.com>
2022-07-28 10:30:47 -0300

    Add files via upload
    Modificado el tema del largo de la contraseña, solo es 16 si no se elige ninguna opción de las listas o si no ingresa un largo válido para la contraseña

--- password_generator.py
+++ password_generator.py
@@ -1,3 +1,4 @@
+
 import random
 
 mayusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
@@ -11,30 +12,37 @@ op_num = input("Desea que hayan numeros en la clave? S/N: ")
 op_sim = input("Desea que hayan simbolos en la clave? S/N: ")
 
 lista_op = []
-eligio=False
+eligio_may=False
+eligio_min=False
+eligio_num=False
+eligio_sim=False
 if op_may == "S":
     lista_op = lista_op + mayusculas
-    eligio = True
+    eligio_may = True
 if op_min == "S":
     lista_op = lista_op + minusculas
-    eligio = True
+    eligio_min = True
 if op_num == "S":
     lista_op = lista_op + numeros
-    eligio = True
+    eligio_num = True
 if op_sim == "S":
     lista_op = lista_op + simbolos
-    eligio = True
-    
-cant = input("Ingrese la cantidad de cifras de la contraseña: ")
-while cant.isdigit() == False:
-    print(f"La cantidad debe de ser un número entero")
-    cant = input("Ingrese la cantidad de cifras de la contraseña: ")
+    eligio_sim = True
+
+try:    
+    cant = int(input("Ingrese la cantidad de cifras de la contraseña (0 o caracter para generar largo por defecto): "))
+except:
+    cant = 16
 else:
-    cant = int(cant)
-   
+    if cant == 0:
+        cant = 16
+
+
+  
 # Corregir el condicional, ya que siempre me devuelve una contraseña de 16 digitos
-if eligio != True:
+if eligio_may != True and eligio_min != True and eligio_num != True and eligio_sim != True:
     lista_op = lista_op + mayusculas + minusculas + numeros
+    cant = 16
 
 contrasenia = ''
 
@@ -43,4 +51,3 @@ contrasenia = ''
 for i in range(1,cant+1):
     contrasenia = contrasenia + random.choice(lista_op)
 print(f"La contraseña quedo: {contrasenia}")
-
